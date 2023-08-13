20c552abfcb1942d8b3cd0bae09ceaa352498020
EnriqueWidjaja <136328899+EnriqueWidjaja@users.noreply.github.com>
2023-08-13 15:49:13 +0700

    CalculatorTest.py

--- CalculatorTest/CalculatorTest/CalculatorTest.py
+++ CalculatorTest/CalculatorTest/CalculatorTest.py
@@ -6,35 +6,43 @@ root = Tk()
 root.title ('Calculator-Test')
 root.resizable(0,0)
 
+#buat display angka dan proses
 display = Entry(root)
 display.grid(row=1,columnspan=6,sticky="nesw")
 
+#tombol 789
 Button(root,text="7",command = lambda :get_variables(7)).grid(row=2,column=0, sticky="nesw")
 Button(root,text=" 8 ",command = lambda :get_variables(8)).grid(row=2,column=1, sticky="nesw")
 Button(root,text=" 9 ",command = lambda :get_variables(9)).grid(row=2,column=2, sticky="nesw")
- 
+
+#tombol 456
 Button(root,text="4",command = lambda :get_variables(4)).grid(row=3,column=0, sticky="nesw")
 Button(root,text=" 5 ",command = lambda :get_variables(5)).grid(row=3,column=1, sticky="nesw")
 Button(root,text=" 6 ",command = lambda :get_variables(6)).grid(row=3,column=2, sticky="nesw")
- 
+
+#tombol 123
 Button(root,text="1",command = lambda :get_variables(1)).grid(row=4,column=0, sticky="nesw")
 Button(root,text=" 2 ",command = lambda :get_variables(2)).grid(row=4,column=1, sticky="nesw")
 Button(root,text=" 3 ",command = lambda :get_variables(3)).grid(row=4,column=2, sticky="nesw")
- 
+
+#tombol 0.AC
 Button(root,text=" 0 ",command = lambda :get_variables(0)).grid(row=5,column=0, sticky="nesw")
 Button(root,text=" . ",command = lambda :get_variables(".")).grid(row=5, column=1, sticky="nesw")
 Button(root,text="AC",command = lambda :clear_all()).grid(row=5,column=2, sticky="nesw")
 
+#tombol operator +-*/
 Button(root,text="+",command= lambda :get_operation("+")).grid(row=2,column=3, sticky="nesw")
 Button(root,text=" - ",command= lambda :get_operation("-")).grid(row=3,column=3, sticky="nesw")
 Button(root,text=" * ",command= lambda :get_operation("*")).grid(row=4,column=3, sticky="nesw")
 Button(root,text=" / ",command= lambda :get_operation("/")).grid(row=5,column=3, sticky="nesw")
 
+#tambahan tombol buat pi, persen, buka kurung, eksponen
 Button(root,text="pi",command= lambda :get_operation("*3.14")).grid(row=2,column=4, sticky="nesw")
 Button(root,text=" % ",command= lambda :get_operation("%")).grid(row=3,column=4, sticky="nesw")
 Button(root,text=" ( ",command= lambda :get_operation("(")).grid(row=4,column=4, sticky="nesw")
 Button(root,text=" exp ",command= lambda :get_operation("**")).grid(row=5,column=4, sticky="nesw")
- 
+
+#tombol delete, faktorial, tutup kurung, pangkat 2, sama dengan
 Button(root,text="<-",command= lambda :undo()).grid(row=2,column=5, sticky="nesw")
 Button(root,text=" x! ", command= lambda: fact()).grid(row=3,column=5, sticky="nesw")
 Button(root,text=" ) ",command= lambda :get_operation(")")).grid(row=4,column=5, sticky="nesw")
@@ -43,20 +51,24 @@ Button(root,text="=",command= lambda :calculate()).grid(columnspan=6, sticky="ne
 
 i = 0
 
+#buat dapet angka
 def get_variables(num):
     global i
     display.insert(i,num)
     i+=1
 
+#buat dapet operator
 def get_operation(operator):
     global i
     length = len(operator)
     display.insert(i,operator)
     i+=length
 
+#buat apus semua
 def clear_all():
     display.delete(0,END)
 
+#buat delete
 def undo():
     entire_string = display.get()
     if len(entire_string):
@@ -67,6 +79,7 @@ def undo():
         clear_all()
         display.insert(0,"Error")
 
+#buat ngitung
 def calculate():
     entire_string = display.get()
     try:
@@ -78,6 +91,7 @@ def calculate():
         clear_all()
         display.insert(0,"Error")
 
+#buat faktorial
 def fact():
     entire_string = display.get()
     try:
